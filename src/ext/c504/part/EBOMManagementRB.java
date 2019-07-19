a51c037244b1c0c207db410bb4622d73cb7c744f
payne <wxyy1990@163.com>
2019-07-19 23:52:21 +0800

    Create EBOMManagementRB.java

---
+++ src/ext/c504/part/EBOMManagementRB.java
@@ -0,0 +1,17 @@
+package ext.c504.part;
+
+import java.util.ListResourceBundle;
+
+public class EBOMManagementRB extends ListResourceBundle {
+
+	static final Object[][] contents = {
+		 {"CPart.downLoadDoc.tooltip", "обть" },
+		 {"CPart.downLoadDoc.description", "обть" },
+		 {"CPart.downLoadDoc.icon", "download.gif" },
+		 {"CPart.downLoadDoc.title", "обть" }
+	};
+
+	public Object[][] getContents() {
+		return contents;
+	}
+}
